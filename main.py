a136a000911effb60581e4fea99562c8f4127e61
stefan-bordei <stefan.bordei13@gmail.com>
2022-12-06 17:39:13 +0000

    refactoring print

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ from src.y2022 import day1, day2, day3, day4, day5, day6
 
 
 DAY_MAP = {
-    "22": {
-        "1": day1,
-        "2": day2,
-        "3": day3,
-        "4": day4,
-        "5": day5,
-        "6": day6
-    }
+    "1": day1,
+    "2": day2,
+    "3": day3,
+    "4": day4,
+    "5": day5,
+    "6": day6
+}
+
+
+YEAR_MAP = {
+    "22": DAY_MAP
 }
 
 
@@ -21,6 +24,13 @@ if __name__ == '__main__':
     parser.add_argument('--day', help='Day of AoC year.')
     
     args = parser.parse_args()
+    
+    if not args.day:
+        for k in DAY_MAP.keys():
+            print(f'\n---Year:{args.year} | Day:{k}---')
+            YEAR_MAP[args.year][k].solve()
+    else:
+        print(f'\n---Year:{args.year} | Day:{args.day}---')
+        YEAR_MAP[args.year][args.day].solve()
 
-    DAY_MAP[args.year][args.day].solve()
 
